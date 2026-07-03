924484eb5ebb7f8b7751c413468eba236128346f
Abhishek Kumar <kabhishek10266@gmail.com>
2026-07-03 10:39:54 +0530

    Added Variables programs

---
+++ Variables/Age.java
@@ -0,0 +1,9 @@
+public class Age {
+    public static void main(String[] args)
+    {
+        int age =25;
+        String name = "Abhishek";
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
