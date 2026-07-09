1b98bfbd7acaa34642389eb9834d4ddac21c297c
Abhishek Kumar <kabhishek10266@gmail.com>
2026-07-09 11:26:21 +0530

    Trying some question

---
+++ Input SC/Question1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Question1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);   //Scanner is a class and next is a method of scanner class which is used to take input from user.
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+        System.out.println("Your Name is: " + name);
+        System.out.println("Your Age is: " + age);
+
+
+    }
+}
